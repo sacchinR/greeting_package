7bf77ae6131274dc781b430ac5176b3fd19df5ee
sacchinR <sacchinrathinasamy@gmail.com>
2024-12-09 22:32:01 +0530

    monarch-cats greets

--- src/morningGreet.ts
+++ src/morningGreet.ts
@@ -1,3 +1,9 @@
-export const morningGreet = (name: String) => {
-  return "Good Morning " + name;
+const monarchCats = ["Garfield", "Felix"];
+
+export const morningGreet = (name: string) => {
+  let greet = "Good Morning ";
+  if (monarchCats.includes(name)) {
+    greet = "Good Morning, your MAJESTY, ";
+  }
+  return greet + name;
 };
